7a727a1d584d5e5e5fcf1164155da4eb9ca458d5
William Barnes <barnesw91291@gmail.com>
2017-02-12 18:58:35 -0800

    datatypes and comments

--- basics/test.py
+++ basics/test.py
@@ -1,1 +1,14 @@
 print ("hello, Python")
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+
+# printing array of days to the console
+print (days)
+
+# input in terminal will error out
+input("\n\nPress the enter key to exit.")
