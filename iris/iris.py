368c044e9f8a3376c5dbddc43a829260dd6c9d89
William Barnes <barnesw91291@gmail.com>
2017-02-12 20:28:58 -0800

    showing iris data plots

--- iris/iris.py
+++ iris/iris.py
@@ -17,3 +17,27 @@ from sklearn.svm import SVC
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
+
+# shape
+print dataset.shape
+
+#head
+print dataset.head(20)
+
+#descriptions
+print dataset.describe()
+
+# class distribution
+print dataset.groupby('class').size()
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
