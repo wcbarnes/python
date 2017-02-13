8aee361b6cc76192217bb9b095709b3fb9a5ed70
William Barnes <barnesw91291@gmail.com>
2017-02-12 19:05:59 -0800

    variable types

--- basics/test.py
+++ basics/test.py
@@ -11,4 +11,84 @@ made up of multiple lines and sentences."""
 print (days)
 
 # input in terminal will error out
-input("\n\nPress the enter key to exit.")
+# input("\n\nPress the enter key to exit.")
+
+import sys; x = 'foo'; sys.stdout.write(x + '\n')
+
+hundred = 100
+floating = 100.5
+name = "will"
+
+print (hundred)
+print (floating)
+print (name)
+
+
+
+# multiple variable assignment
+a = b = c = 1
+
+print (a)
+print (b)
+
+b = 2
+
+print (a)
+print (b)
+
+a, b, c = 1, 2, "john"
+
+print (a)
+print (b)
+print (c)
+
+# strings
+
+str = 'Hello World!'
+print (str)          # Prints complete string
+print (str[0])       # Prints first character of the string
+print (str[2:5])     # Prints characters starting from 3rd to 5th
+print (str[2:])      # Prints string starting from 3rd character
+print (str * 2)      # Prints string two times
+print (str + "TEST") # Prints concatenated string
+
+
+#lists
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+
+print (list)          # Prints complete list
+print (list[0])       # Prints first element of the list
+print (list[1:3])     # Prints elements starting from 2nd till 3rd 
+print (list[2:])      # Prints elements starting from 3rd element
+print (tinylist * 2)  # Prints list two times
+print (list + tinylist) # Prints concatenated lists
+
+
+#tuples
+
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
+tinytuple = (123, 'john')
+
+print (tuple)           # Prints complete tuple
+print (tuple[0])        # Prints first element of the tuple
+print (tuple[1:3])      # Prints elements starting from 2nd till 3rd 
+print (tuple[2:])       # Prints elements starting from 3rd element
+print (tinytuple * 2)   # Prints tuple two times
+print (tuple + tinytuple) # Prints concatenated tuple
+
+# dictionarys / objects
+
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+print (dict['one'])       # Prints value for 'one' key
+print (dict[2])           # Prints value for 2 key
+print (tinydict)          # Prints complete dictionary
+print (tinydict.keys())   # Prints all the keys
+print (tinydict.values()) # Prints all the values
